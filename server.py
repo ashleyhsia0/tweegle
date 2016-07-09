a01d0962fb33d5a648b20949ce2803b91d758d7c
Ashley Hsiao <aiyihsiao@gmail.com>
2016-07-09 15:12:07 -0700

    Add comments for readability

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ from flask import Flask, render_template, redirect, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 import os
-
 import tweepy
 
 import operator
 
+# Set up Tweepy (Twitter API library)
 auth = tweepy.OAuthHandler(os.environ["TWITTER_CONSUMER_KEY"],
                            os.environ["TWITTER_CONSUMER_SECRET"])
 auth.set_access_token(os.environ["TWITTER_ACCESS_TOKEN_KEY"],
@@ -16,6 +16,7 @@ auth.set_access_token(os.environ["TWITTER_ACCESS_TOKEN_KEY"],
 
 twitter_api = tweepy.API(auth)
 
+# Set up Flask web app
 app = Flask(__name__)
 app.secret_key = "ABC123"
 
@@ -46,6 +47,7 @@ def show_login():
             hashtag = item['text']
             hashtags[hashtag] = hashtags.get(hashtag, 0) + 1
 
+    # Sort hashtags based on count in ascending order
     sorted_hashtags = sorted(hashtags.items(), key=operator.itemgetter(1), reverse=True)
 
     return render_template("search_results.html",
