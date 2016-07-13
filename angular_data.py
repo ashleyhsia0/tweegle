29603a5a89b6a481fda1fc9c40b140324245e825
Ashley Hsiao <aiyihsiao@gmail.com>
2016-07-13 01:10:21 -0700

    Create functions to make JSONs for users and hashtags

--- angular_data.py
+++ angular_data.py
@@ -13,13 +13,51 @@ def jsonify_tweets(tweets):
             "userName": tweet.user.name,
             "userHandle": tweet.user.screen_name,
             "userProfileImg": tweet.user.profile_image_url,
-            "tweetId": tweet.id_str,
-            "tweetContent": tweet.text,
-            "tweetDate": (tweet.created_at).isoformat(),  # Create serialized version for datetime object
-            "tweetRetweets": tweet.retweet_count,
-            "tweetFavorites": tweet.favorite_count
+            "id": tweet.id_str,
+            "text": tweet.text,
+            "date": (tweet.created_at).isoformat(),  # Create serialized version for datetime object
+            "numRetweets": tweet.retweet_count,
+            "numFavorites": tweet.favorite_count
         }
 
         tweets_list.append(tweet_dict)
 
     return json.dumps(tweets_list)
+
+
+def jsonify_users(users):
+    """Return Tweepy user objects as JSON."""
+
+    users_list = []
+
+    for user in users:
+        user_dict = {
+            "name": user.name,
+            "handle": user.screen_name,
+            "profileImg": user.profile_image_url,
+            "location": user.location,
+            "description": user.description,
+            "numTweets": user.statuses_count,
+            "numFollowers": user.followers_count,
+            "numFollowing": user.friends_count
+        }
+
+        users_list.append(user_dict)
+
+    return json.dumps(users_list)
+
+
+def jsonify_hashtags(hashtags):
+    """Return list of hashtags as JSON."""
+
+    hashtags_list = []
+
+    for hashtag, count in hashtags:
+        hashtag_dict = {
+            "name": hashtag,
+            "count": count
+        }
+
+        hashtags_list.append(hashtag_dict)
+
+    return json.dumps(hashtags_list)
