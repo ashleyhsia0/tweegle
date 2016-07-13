aec3f5705f0603119120a9570e85d851ed93489b
Ashley Hsiao <aiyihsiao@gmail.com>
2016-07-13 02:22:33 -0700

    Change to one controller

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,17 +1,9 @@
 "use strict";
 
-myApp.controller('tweetsController', function ($scope) {
-    console.log($scope.tweets);
-});
-
-myApp.controller('usersController', function ($scope) {
+myApp.controller('myController', function ($scope) {
     $scope.slicedTwoUsers = [];
     while ($scope.users.length) {
         $scope.slicedTwoUsers.push($scope.users.splice(0, 2));
     }
-});
-
-myApp.controller('hashtagsController', function ($scope) {
-    
 });
 
